574eeb0e9522111d3b6f35da2d0dfe112551dd3e
IvanBalyasnikov <balalaika22813337@gmail.com>
2024-09-25 11:07:58 +0400

    add hash password

--- src/api/auth.py
+++ src/api/auth.py
@@ -1,4 +1,6 @@
 from fastapi import APIRouter, Body
+from passlib.context import CryptContext
+
 from src.repositories.users import UsersRepository
 from src.schemas.users import SUserAdd, SUserRequestAdd
 from src.database import async_session_maker
@@ -9,11 +11,14 @@ router = APIRouter(prefix="/users",
                    tags = ["Аунтефикация "])
 
 
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+
 @router.post("/register")
 async def register_user(
     user_data:SUserRequestAdd = Body(description="Информация для регистрации пользователей", openapi_examples=add_examples)
     ):
-    hashed_password = user_data.password
+    hashed_password = pwd_context.hash(user_data.password)
     new_user_data = SUserAdd(email=user_data.email, hashed_password=hashed_password)
     async with async_session_maker() as session:
         await UsersRepository(session).add(new_user_data)
