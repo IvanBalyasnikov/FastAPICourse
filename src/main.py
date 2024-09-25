b1e15f3ad4a04e9a2040cf4dc1557c91b7449314
IvanBalyasnikov <balalaika22813337@gmail.com>
2024-09-25 11:31:37 +0400

    docs fix

--- src/main.py
+++ src/main.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI
 import uvicorn
 import sys
 from pathlib import Path
+from fastapi.openapi.docs import get_swagger_ui_html
+
 
 sys.path.append(str(Path(__file__).parent.parent))
 
@@ -9,10 +11,22 @@ sys.path.append(str(Path(__file__).parent.parent))
 from src.api.hotels import router as hotels_router
 from src.api.auth import router as auth_router
 
+
+
 app = FastAPI()
 
 app.include_router(auth_router)
 app.include_router(hotels_router)
+
+@app.get("/docs", include_in_schema=False)
+async def custom_swagger_ui_html():
+    return get_swagger_ui_html(
+        openapi_url=app.openapi_url,
+        title=app.title + " - Swagger UI",
+        oauth2_redirect_url=app.swagger_ui_oauth2_redirect_url,
+        swagger_js_url="https://unpkg.com/swagger-ui-dist@5/swagger-ui-bundle.js",
+        swagger_css_url="https://unpkg.com/swagger-ui-dist@5/swagger-ui.css",
+    )
 
 
 
