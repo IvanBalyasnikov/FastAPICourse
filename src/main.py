1c7602f77c8cd1bd4ae9459c7f5fb1cae65de301
IvanBalyasnikov <balalaika22813337@gmail.com>
2024-09-25 11:01:12 +0400

    fix naming of auth

--- src/main.py
+++ src/main.py
@@ -8,11 +8,12 @@ sys.path.append(str(Path(__file__).parent.parent))
 
 
 from src.api.hotels import router as hotels_router
-from src.api.users import router as users_router
+from src.api.auth import router as auth_router
 
 app = FastAPI()
+
+app.include_router(auth_router)
 app.include_router(hotels_router)
-app.include_router(users_router)
 
 
 
