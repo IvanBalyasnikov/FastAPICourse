ff915c9a7f179706f666c80ca86b9f73c8639ceb
IvanBalyasnikov <balalaika22813337@gmail.com>
2024-09-25 10:56:17 +0400

    add users examples

--- src/api/users.py
+++ src/api/users.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, Body, Query
 from src.repositories.users import UsersRepository
 from src.schemas.users import SUserAdd, SUserRequestAdd
 from src.database import async_session_maker
+from src.api.users_examples import add_examples
 
 
 router = APIRouter(prefix="/users",
@@ -10,7 +11,7 @@ router = APIRouter(prefix="/users",
 
 @router.post("/register")
 async def register_user(
-    user_data:SUserRequestAdd
+    user_data:SUserRequestAdd = Body(description="Информация для регистрации пользователей", openapi_examples=add_examples)
     ):
     hashed_password = user_data.password
     new_user_data = SUserAdd(email=user_data.email, hashed_password=hashed_password)
---
+++ src/api/users_examples.py
@@ -0,0 +1,22 @@
+add_examples = {
+    1:{
+        'description':"Регистрация пользователя",
+         'value':
+         {"email": "asldj@mail.ru",
+          "password":"askdjjl123"}
+    },
+    2:{
+        'description':"Регистрация пользователя",
+         'value':
+         {"email": "123456789@mail.ru",
+          "password":"skdfjlkj42"}
+    },
+    3:{
+        'description':"Регистрация пользователя",
+         'value':
+         {"email": "abcde@mail.ru",
+          "password":"123456789dfg"}
+    },
+}
+
+
